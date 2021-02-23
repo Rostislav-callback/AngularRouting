1e73efd00e019ff06a455921d2d1e9206a682f17
Rostislav-callback <rost33445500@gmail.com>
2021-02-23 15:46:23 +0200

    at-1-post-and-validation-email

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators, ValidationErrors } from '@angular/forms';
+import { FormGroup, Validators, ValidationErrors, FormBuilder, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-signup',
@@ -12,19 +13,32 @@ export class SignupComponent implements OnInit {
 
   public validators = [Validators.required];
 
-  public signupForm = new FormGroup({
-    password: new FormGroup({
-      password: new FormControl('', this.validators),
-      repeatPassword: new FormControl('', this.validators),
+  public signupForm = this.fb.group({
+    //username: ['', this.validators],
+    email: this.fb.group({
+      email: ['', this.validators]
+    }, {
+      validators: [this.emailValidator]
+    }), 
+    password: this.fb.group({
+      password: ['', this.validators],
+      repeatPassword: ['', this.validators]
     }, {
       validators: [this.equalValidator]
     })
   });
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private fb: FormBuilder,
+              private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+
+  postEmailData(signupForm: FormGroup){
+          
+    return this.http.post('http://localhost:4200/home', signupForm.value); 
+}
 
   public signup(FormValue: any): void {
       this.router.navigate(['/home']);
@@ -35,5 +49,17 @@ export class SignupComponent implements OnInit {
     return password === repeatPassword ? null : {
       'Password' : 'Non working'
     }
+  }
+
+  public emailValidator(control: FormGroup): ValidationErrors | null {
+    const [email] = Object.values(control.value); 
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email)) ? null : {
+      'Password' : 'Non working'
+    }
+  }
+
+  public emailAtHome({value: email}: FormControl): ValidationErrors | null {
+    return 
   }
 }
