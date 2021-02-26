dcf3ab95dcb950f4f676d991d626da052b3bbbef
Rostislav-callback <rost33445500@gmail.com>
2021-02-26 14:26:09 +0200

    validate-login-page-with-cancel

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, Validators, ValidationErrors, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,21 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  public validators = [Validators.required];
+
+  public loginForm = this.fb.group({
+    email: this.fb.group({
+      email: ['', this.validators]
+    }, {
+      validators: [this.emailValidator]
+    }), 
+    password: this.fb.group({
+      password: ['', this.validators]
+    })
+  });
+
+  constructor(private router: Router,
+              private fb: FormBuilder,) { }
 
   ngOnInit(): void {
   }
@@ -16,4 +31,12 @@ export class LoginComponent implements OnInit {
   public login(FormValue: any): void {
     this.router.navigate(['/home']);
   } 
+
+  public emailValidator(control: FormGroup): ValidationErrors | null {
+    const [email] = Object.values(control.value); 
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email)) ? null : {
+      'Email' : 'Non working'
+    }
+  }
 }
